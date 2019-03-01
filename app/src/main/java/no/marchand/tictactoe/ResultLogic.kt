b32718d3f2aae56631221938510e4cfe61522270
Chris FFRR <chris@shotatdawn.net>
2019-03-01 19:46:16 +0100

    refactored some warnings

--- app/src/main/java/no/marchand/tictactoe/ResultLogic.kt
+++ app/src/main/java/no/marchand/tictactoe/ResultLogic.kt
@@ -7,68 +7,61 @@ var timesPlayed = 0
 fun didWin() {
 
 
-    if (timesPlayed < 8) {
+    if (timesPlayed < 7) {
         for (col in 0..2) {
             for (i in 0..2) {
 
-                /*
-                111
-                000
-                000
-                 */
-                if (board[0][0] === 1 && board[0][1] === 1 && board[0][2] === 1) {
+                //Check for 3 in a row horizontal first row - X or O
+                if (board[0][0].equals(1) && board[0][1].equals(1) && board[0][2].equals(1)) {
+                    Log.d("debug", "player one wins")
+                } else if (board[0][0].equals(2) && board[0][1].equals(2) && board[0][2].equals(2)) {
                     Log.d("debug", "player one wins")
                 }
-                /*
-                000
-                111
-                000
-                 */
-                if (board[1][0] === 1 && board[1][1] === 1 && board[1][2] === 1) {
-                    Log.d("debug", "player one wins")
-                }
-                /*
-                000
-                000
-                111
-                 */
-                if (board[2][0] === 1 && board[2][1] === 1 && board[2][2] === 1) {
-                    Log.d("debug", "player one wins")
-                }
-                /*
-                100
-                100
-                100
-                 */
-                if (board[0][0] === 1 && board[1][0] === 1 && board[2][0] === 1) {
-                    Log.d("debug", "player one wins")
-                }
-                /*
-                010
-                010
-                010
-                 */
-                if (board[0][1] === 1 && board[1][1] === 1 && board[2][1] === 1) {
-                    Log.d("debug", "player one wins")
-                }
-                /*
-                001
-                001
-                001
-                 */
-                if (board[0][2] === 1 && board[1][2] === 1 && board[2][2] === 1) {
-                    Log.d("debug", "player one wins")
-                }
-                /*
-               100   001
-               010 & 010
-               001   100
-                */
 
-                if ((board[0][0] === 1 && board[1][1] === 1 && board[2][2] === 1)
-                    || (board[0][2] === 1 && board[1][1] === 1 && board[2][0] === 1)
+                //Check for 3 in a row horizontal second row - X or O
+                if (board[1][0].equals(1) && board[1][1].equals(1) && board[1][2].equals(1)) {
+                    Log.d("debug", "player one wins")
+                } else if (board[1][0].equals(2) && board[1][1].equals(2) && board[1][2].equals(2)) {
+                    Log.d("debug", "player two wins")
+                }
+
+                //Check for 3 in a row horizontal third row - X or O
+                if (board[2][0].equals(1) && board[2][1].equals(1) && board[2][2].equals(1)) {
+                    Log.d("debug", "player one wins")
+                } else if (board[2][0].equals(2) && board[2][1].equals(2) && board[2][2].equals(2)) {
+                    Log.d("debug", "player two wins")
+                }
+
+                //Check for 3 in a row vertical first column - X or O
+                if (board[0][0].equals(1) && board[1][0].equals(1) && board[2][0].equals(1)) {
+                    Log.d("debug", "player one wins")
+                } else if (board[0][0].equals(2) && board[1][0].equals(2) && board[2][0].equals(2)) {
+                    Log.d("debug", "player two wins")
+                }
+
+                //Check for 3 in a row vertical second column - X or O
+                if (board[0][1].equals(1) && board[1][1].equals(1) && board[2][1].equals(1)) {
+                    Log.d("debug", "player one wins")
+                } else if (board[0][1].equals(2) && board[1][1].equals(2) && board[2][1].equals(2)) {
+                    Log.d("debug", "player two wins")
+                }
+
+                //Check for 3 in a row vertical third column - X or O
+                if (board[0][2].equals(1) && board[1][2].equals(1) && board[2][2].equals(1)) {
+                    Log.d("debug", "player one wins")
+                } else if (board[0][2].equals(2) && board[1][2].equals(2) && board[2][2].equals(2)) {
+                    Log.d("debug", "player two wins")
+                }
+
+                //Check for 3 in a row horizontal - X or O
+                if (board[0][0].equals(1) && board[1][1].equals(1) && board[2][2].equals(1)
+                    || (board[0][2].equals(1) && board[1][1].equals(1) && board[2][0].equals(1))
                 ) {
                     Log.d("debug", "player one wins")
+                } else if (board[0][0].equals(2) && board[1][1].equals(2) && board[2][2].equals(2)
+                    || (board[0][2].equals(2) && board[1][1].equals(2) && board[2][0].equals(2))
+                ) {
+                    Log.d("debug", "player two wins")
                 }
             }
         }
