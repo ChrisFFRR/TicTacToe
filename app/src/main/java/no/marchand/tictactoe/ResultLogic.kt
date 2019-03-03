18f070f0de467d8e32ae84431a5734917c4be917
chrisFFRR <chris@shotatdawn.net>
2019-03-03 15:58:48 +0100

    refactored to fragment instead of mainactivity

--- app/src/main/java/no/marchand/tictactoe/ResultLogic.kt
+++ app/src/main/java/no/marchand/tictactoe/ResultLogic.kt
@@ -4,64 +4,77 @@ import android.util.Log
 
 var timesPlayed = 0
 
+
 fun didWin(): Int {
 
 
-    if (timesPlayed < 7) {
+    if (timesPlayed < 7 && gameInProgress) {
         for (col in 0..2) {
             for (i in 0..2) {
 
                 //Check for 3 in a row horizontal first row - X or O
                 if (board[0][0].equals(1) && board[0][1].equals(1) && board[0][2].equals(1)) {
                     Log.d("debug", "player one wins")
+                    //gameInProgress = false
                     return 1
                 } else if (board[0][0].equals(2) && board[0][1].equals(2) && board[0][2].equals(2)) {
                     Log.d("debug", "player one wins")
+                   // gameInProgress = false
                     return 2
                 }
 
                 //Check for 3 in a row horizontal second row - X or O
                 if (board[1][0].equals(1) && board[1][1].equals(1) && board[1][2].equals(1)) {
                     Log.d("debug", "player one wins")
+                   // gameInProgress = false
                     return 1
                 } else if (board[1][0].equals(2) && board[1][1].equals(2) && board[1][2].equals(2)) {
                     Log.d("debug", "player two wins")
+                    //gameInProgress = false
                     return 2
                 }
 
                 //Check for 3 in a row horizontal third row - X or O
                 if (board[2][0].equals(1) && board[2][1].equals(1) && board[2][2].equals(1)) {
                     Log.d("debug", "player one wins")
+                    //gameInProgress = false
                     return 1
                 } else if (board[2][0].equals(2) && board[2][1].equals(2) && board[2][2].equals(2)) {
                     Log.d("debug", "player two wins")
+                    //gameInProgress = false
                     return 2
                 }
 
                 //Check for 3 in a row vertical first column - X or O
                 if (board[0][0].equals(1) && board[1][0].equals(1) && board[2][0].equals(1)) {
                     Log.d("debug", "player one wins")
+                   // gameInProgress = false
                     return 1
                 } else if (board[0][0].equals(2) && board[1][0].equals(2) && board[2][0].equals(2)) {
                     Log.d("debug", "player two wins")
+                   // gameInProgress = false
                     return 2
                 }
 
                 //Check for 3 in a row vertical second column - X or O
                 if (board[0][1].equals(1) && board[1][1].equals(1) && board[2][1].equals(1)) {
                     Log.d("debug", "player one wins")
+                    //gameInProgress = false
                     return 1
                 } else if (board[0][1].equals(2) && board[1][1].equals(2) && board[2][1].equals(2)) {
                     Log.d("debug", "player two wins")
+                   // gameInProgress = false
                     return 2
                 }
 
                 //Check for 3 in a row vertical third column - X or O
                 if (board[0][2].equals(1) && board[1][2].equals(1) && board[2][2].equals(1)) {
                     Log.d("debug", "player one wins")
+                   // gameInProgress = false
                     return 1
                 } else if (board[0][2].equals(2) && board[1][2].equals(2) && board[2][2].equals(2)) {
                     Log.d("debug", "player two wins")
+                    //gameInProgress = false
                     return 2
                 }
 
@@ -70,11 +83,13 @@ fun didWin(): Int {
                     || (board[0][2].equals(1) && board[1][1].equals(1) && board[2][0].equals(1))
                 ) {
                     Log.d("debug", "player one wins")
+                   // gameInProgress = false
                     return 1
                 } else if (board[0][0].equals(2) && board[1][1].equals(2) && board[2][2].equals(2)
                     || (board[0][2].equals(2) && board[1][1].equals(2) && board[2][0].equals(2))
                 ) {
                     Log.d("debug", "player two wins")
+                   // gameInProgress = false
                     return 2
                 }
             }
@@ -84,6 +99,7 @@ fun didWin(): Int {
         return 0
     } else {
         Log.d("debug", "draw")
+       // gameInProgress = false
         return 3
         timesPlayed = 0
     }
