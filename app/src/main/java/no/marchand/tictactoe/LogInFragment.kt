99101edebeb1f36c9ad4910944c04cb5827cd02e
Chris FFRR <chris@shotatdawn.net>
2019-03-05 13:06:04 +0100

    Small changes to LoginFragment

--- app/src/main/java/no/marchand/tictactoe/LogInFragment.kt
+++ app/src/main/java/no/marchand/tictactoe/LogInFragment.kt
@@ -2,14 +2,17 @@ package no.marchand.tictactoe
 
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import kotlinx.android.synthetic.main.log_in_fragment.*
 
 
 class LogInFragment: Fragment() {
@@ -28,10 +31,15 @@ class LogInFragment: Fragment() {
 
         playBtn.setOnClickListener {
 
-            editor.putString("UserName", userNameTxtView.text.toString())
+            editor.putString("UserName", editTextUserName.text.toString())
             editor.commit()
+            Log.d("DEBUG", "CLICK")
+            Log.d("DEBUG", userNameTxtView.text.toString())
 
+            val goToGame = Intent()
 
+            goToGame.setClass(activity, MainActivity::class.java)
+            startActivity(goToGame)
 
 
         }
