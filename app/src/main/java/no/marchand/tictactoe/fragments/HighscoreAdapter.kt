0fba73281dce5a7e1ec15477491435ceaa8804bd
Chris FFRR <chris@shotatdawn.net>
2019-03-16 17:28:52 +0100

    deleted unused imports, moved some global fields to classfield

--- app/src/main/java/no/marchand/tictactoe/fragments/HighscoreAdapter.kt
+++ app/src/main/java/no/marchand/tictactoe/fragments/HighscoreAdapter.kt
@@ -1,17 +1,14 @@
 package no.marchand.tictactoe.fragments
 
 
-import android.app.Application
+
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TableRow
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import no.marchand.tictactoe.R
-import no.marchand.tictactoe.highscoreDb.HighscoreRepository
-import no.marchand.tictactoe.highscoreDb.HighscoreViewModel
 import no.marchand.tictactoe.highscoreDb.User
 
 class HighscoreAdapter internal constructor (context: Context) : RecyclerView.Adapter<HighscoreAdapter.HighscoreHolder>() {
@@ -21,7 +18,7 @@ class HighscoreAdapter internal constructor (context: Context) : RecyclerView.Ad
 
 
     inner class HighscoreHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        //val userPos: TextView = itemView.findViewById(R.id.highScorePositionView)
+
         val userName: TextView = itemView.findViewById(R.id.highScorePlayerNameView)
         val userTime: TextView = itemView.findViewById(R.id.highscoreTimeView)
     }
@@ -35,10 +32,10 @@ class HighscoreAdapter internal constructor (context: Context) : RecyclerView.Ad
      return users.size
     }
 
-    //Sikkert feil
+
     override fun onBindViewHolder(holder: HighscoreHolder, pos: Int) {
       val current = users[pos]
-        //holder.userPos.text = current.id.toString()
+
         holder.userName.text = current.user
         holder.userTime.text = current.time.toString() + "  " + "seconds"
     }
